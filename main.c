358f7c8eb03e98c56807c7a13ee2becc6341017f
angelql11 <65978443+angelql11@users.noreply.github.com>
2020-07-03 16:55:45 -0400

    Add files via upload

--- main.c
+++ main.c
@@ -21,28 +21,32 @@ struct level{
     int id;
 };
 
-
 typedef struct mapa{
     level * root;
     level  * current;
 }Mapa;
 
 typedef struct jugador{
-
-    int id;
-    int def;
+    int dfs;
     int atk;
     char * nombre;
-
+    int level;
 }jugador;
 
 typedef struct enemigo{
-
     int prt;
     int atk;
     char *nombre;
-
 }enemigo;
+
+jugador *crear_jugador (int level ,char *nombre, int atak,int dfs){
+    jugador *p = malloc(sizeof(jugador));
+    p->level = level;
+    p->nombre = nombre;
+    p->atk = atak;
+    p->dfs = dfs;
+    return p;
+}
 
 void typewriter(int x, int y, int velocidad, WINDOW* ventana, char* text){
     int i;
@@ -74,23 +78,17 @@ int menu_principal(){
     int yMax , xMax;
     getmaxyx(stdscr, yMax , xMax);
 
-
     WINDOW * menuwin = newwin(6, xMax-12,yMax-8, 5);
     int yMac, xMac;
     getmaxyx(menuwin,yMac,xMac);
-    //WINDOW * submen = newwin(8,5,5,12);
     showtitle(1,5,1);
 
     box(menuwin,0,0);
-    //box(submen,0,0);
     refresh();
     wrefresh(menuwin);
-    //wrefresh(submen);
 
     keypad(menuwin,true);
-    int y =1;
-
-    
+    int y =1; 
 
     if (has_colors()){
         
@@ -113,7 +111,6 @@ int menu_principal(){
         curs_set(0); 
         
         do{
-            
         	key = wgetch(menuwin);
         	wrefresh(menuwin);
 
@@ -132,15 +129,13 @@ int menu_principal(){
         	}
         	
         	if(key == KEY_DOWN){
-        		mvwprintw(menuwin,y,1," ");
-                y++;
-                if(y==0)y=3;
-                if(y==4)y=1;
-
-                wmove(menuwin,y,1);
-                mvwprintw(menuwin,y,1,">");
-                
-                continue;
+        	  mvwprintw(menuwin,y,1," ");
+                 y++;
+                 if(y==0)y=3;
+                 if(y==4)y=1;
+                 wmove(menuwin,y,1);
+                 mvwprintw(menuwin,y,1,">");
+                 continue;
         	}
 
         }while(key != ENTER);
@@ -160,6 +155,8 @@ void ingresar(char *nombre){
     fputs("100", nuevoJugador);
     fputc(',', nuevoJugador);
     fputs("100", nuevoJugador);
+    fputc(',', nuevoJugador);
+    fputs("1", nuevoJugador);
     fputs("\n", nuevoJugador);
     fclose(nuevoJugador);
 }
@@ -168,14 +165,48 @@ void nueva_partida(){
     
     echo();
     char cadena[128];
-    typewriter(10,10,50,stdscr,"Cual es tu nombre? ");
-    move(15,10);
+    typewriter(9,23,50,stdscr,"¿Cual es tu nombre? ");
+    move(10,23);
     scanw("%[^\n]s",cadena);
-    ingresar(cadena);
-    typewriter(16,10,50,stdscr,"tu nombre es: ");
-    typewriter(17,10,50,stdscr,cadena);
+    typewriter(11,23,50,stdscr,"tu nombre es: ");
+    typewriter(11,37,50,stdscr,cadena);
+    typewriter(12,23,50,stdscr,"estas seguro?");
+    typewriter(14,23,50,stdscr,"SI");
+    typewriter(14,26,50,stdscr,"NO");
     refresh();
+    int key;
+    int x=1;
+    noecho();
 
+    do{
+            
+        key = getch();
+        refresh();
+        	
+        if(key == KEY_LEFT){
+          
+            if(x==1)continue;
+            
+            x=1;
+            mvwprintw(stdscr,15,23,"^");
+            continue;
+        }
+        	
+        if(key == KEY_RIGHT){
+           
+            if(x==3)continue;
+            
+            x=3;
+            mvwprintw(stdscr,15,26,"a");
+            continue;
+        }
+
+    }while(key != ENTER);
+	
+    if(x==3)return nueva_partida();
+
+    ingresar(cadena);
+    jugador *player = crear_jugador(1,cadena,100,100);
 }
 
 int main(int argc, char ** argv){
@@ -189,6 +220,7 @@ int main(int argc, char ** argv){
     clear();
     bkgd(COLOR_PAIR(4));
     switch (option)
+    
     {
     case 1:
         nueva_partida();
@@ -205,12 +237,12 @@ int main(int argc, char ** argv){
         return 0;
     }
     
-   napms(2000);
+    napms(2000);
    
-   refresh();
-   getch();
+    refresh();
+    getch();
    
-   endwin();
+    endwin();
 
     return 0;
 
